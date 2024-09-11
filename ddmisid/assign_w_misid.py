a7982ef88570da13306a5ec2c8322a8e11a107f6
reallyblaised <blaiserd@proton.me>
2024-09-11 10:43:59 -0400

    verification and preprocessing of PID hists in weight assignments against user-defined binning complete

--- ddmisid/assign_w_misid.py
+++ ddmisid/assign_w_misid.py
@@ -1,18 +1,81 @@
 """
 Assign the w_misid weight following the main equation
    
-    w_misid = sum_i Ni/Nred eff_i(h->mu)/eff_i(!mu)
+    w_misid = sum_i Ni/Nref eff_i(h->mu)/eff_i(h->!mu)
 
-see LHCb-ANA-2021-052
+following SLB WG methodology; see LHCb-ANA-2021-052
 """
 
 __author__ = "Blaise Delaney"
 __email__ = "blaise.delaney at cern.ch"
 
 import polars as pl
-from ddmisid.utils import read_config, simple_load
+from ddmisid.utils import read_config, simple_load, load_hist
 import argparse
+import numpy as np
+import boost_histogram as bh
+from typing import Dict
+import warnings
+import pprint
 
+
+def validate_binning(
+    histogram: bh.Histogram, binning: Dict[str, np.ndarray | list]
+) -> None:
+    """Axis validation against the nominal user-defined binning scheme"""
+    for axis in histogram.axes:
+        name = axis.metadata.get("name")
+
+        # verify the axis in the nominal binning scheme
+        if name not in binning:
+            raise ValueError(
+                f"Value '{name}' from metadata is not a valid key in the binning dictionary"
+            )
+
+        # verify edges are as nominal
+        if not np.array_equal(axis.edges, binning[name]):
+            raise ValueError(f"Binning mismatch in '{name}' projection")
+
+
+def validate_n_axes(
+    histogram: bh.Histogram, binning: Dict[str, np.ndarray | list]
+) -> None:
+    """Validate axes multiplicity"""
+    if len(histogram.axes) != len(binning):
+        raise ValueError(
+            f"Histogram has {len(histogram.axes)} axes, but binning specifies {len(binning)} - axes multiplicity mismatch"
+        )
+
+
+def process_pid_hists(
+    opts: argparse.Namespace,
+    binning: Dict[str, np.ndarray | list],
+    verbose: bool = False,
+) -> Dict[str, bh.Histogram]:
+    """Validate PID histogram structure against user-defined nominal binning"""
+    pid_container = {}
+
+    for key, value in vars(opts).items():
+        if value is None:
+            warnings.warn("Warning: None file passed - skipping", UserWarning)
+            continue  # skip [accounting for ghost exclusion]
+
+        if "mu" in key:
+
+            pid_hist = load_hist(value)  # Load muon/antimuon PID efficiencies
+
+            # Validate the binning before adding it to the container
+            validate_n_axes(pid_hist, binning)
+            validate_binning(pid_hist, binning)
+
+            # If test passed, load histograms into container
+            pid_container[key] = pid_hist
+
+    # verbose print of dict
+    if verbose:
+        pprint.pprint(pid_container, width=40, indent=4)
+
+    return pid_container
 
 
 if __name__ == "__main__":
@@ -20,12 +83,49 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="tally all effs, yields and observations for the assignment of w_misid"
     )
+    parser.add_argument("--obs", help="hadron-enriched data .root file")
+    parser.add_argument("--rel_abundances", help="histogram of Ni/Nref")
+    parser.add_argument("--p_to_mu", help="path to proton->mu eff", default=None)
+    parser.add_argument("--pi_to_mu", help="path to pion->mu eff", default=None)
+    parser.add_argument("--k_to_mu", help="path to kaon->mu eff", default=None)
+    parser.add_argument("--e_to_mu", help="path to electron->mu eff", default=None)
+    parser.add_argument("--g_to_mu", help="path to ghost->mu eff", default=None)
+    parser.add_argument(
+        "--p_to_antimu",
+        help="path to proton->!mu eff [ie containted in hadron-enriched data]",
+        default=None,
+    )
+    parser.add_argument(
+        "--pi_to_antimu",
+        help="path to pion->!mu eff [ie containted in hadron-enriched data]",
+        default=None,
+    )
+    parser.add_argument(
+        "--k_to_antimu",
+        help="path to kaon->!mu eff [ie containted in hadron-enriched data]",
+        default=None,
+    )
+    parser.add_argument(
+        "--e_to_antimu",
+        help="path to electron->!mu eff [ie containted in hadron-enriched data]",
+        default=None,
+    )
+    parser.add_argument(
+        "--g_to_antimu",
+        help="path to ghost->!mu eff [ie containted in hadron-enriched data]",
+        default=None,
+    )
     opts = parser.parse_args()
 
-    # binning
+    # user-defined binning
     binning = read_config("config/main.yml", key="pid")["binning"]
 
-    # hadron-enriched observations as lazyframe 
+    # process input in dedicated containers
+    pid_effs = process_pid_hists(opts, binning, False)
+
+    breakpoint()
+
+    # hadron-enriched observations as lazyframe
     data = pl.from_pandas(
         simple_load(
             path=read_config("config/main.yml", key="data")["path"],
